ee5350327af7cf254b9a0b266cf20ba754391b5b
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-02-22 20:00:31 +0530

    completed city repo

---
+++ src/repository/city-repo.js
@@ -0,0 +1,55 @@
+const { City } = require("../models/index");
+
+class CityRepository {
+    async createCity ({ name }) {
+        try {
+            const city= await City.create({
+                name
+            })
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repo");
+            throw {error}
+        }
+    }
+
+    async deleteCity (cityId) {
+        try {
+            await City.destroy({
+                where: {
+                    Id: cityId
+                }
+            })
+            return true;
+        } catch (error) {
+            console.log("Something went wrong in the repo");
+            throw {error}
+        }
+    }
+
+    async updateCity (cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    Id: cityId
+                }
+            })
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repo");
+            throw {error}
+        }
+    }
+
+    async getCity (cityId) {
+        try {
+            const city = await City.findByPk(cityId)
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repo");
+            throw {error}
+        }
+    }
+}
+
+module.exports = CityRepository;
