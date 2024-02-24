372ac6939b624305417b3d94b7218eaf6432e325
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-02-24 16:49:21 +0530

    Added Airplanes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const { PORT } = require("./config/serverConfig")
 
 const ApiRoutes = require("./routes/index")
 
+const db = require('./models/index')
+
 const setUpAndStartServer = async () => {
     const app = express();
 
@@ -14,6 +16,9 @@ const setUpAndStartServer = async () => {
 
     app.listen(PORT, async () => {
         console.log(`Server started at port: ${PORT}`)
+        if(process.env.SYNC_DB) {
+            db.sequelize.sync({alter: true});
+        }
     })
 }
 
